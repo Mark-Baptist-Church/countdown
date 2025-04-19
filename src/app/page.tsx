164fb9f525b097990708095531cca77e55f9330c
Mark Baptist Church <markbaptistav@gmail.com>
2025-04-19 10:42:49 -0500

    feat: added countdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,38 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import styles from "./page.module.css";
 
+const FIVE_MINUTES: number = 60 * 5;
+
 export default function Home() {
+  const [secondsLeft, setSecondsLeft] = useState<number>(FIVE_MINUTES);
+  const [timer, setTimer] = useState<string>(formatTime(FIVE_MINUTES))
+
+  function formatTime(value: number) {
+    const s = value % 60 >= 10 ? value % 60 : `0${value % 60}`;
+    const m = Math.floor(value / 60);
+
+    return `${m}:${s}`;
+  }
+
+  useEffect(() => {
+    if (secondsLeft > 0) {
+      const timerId = setInterval(() => setSecondsLeft(prev => prev - 1), 1000)
+      setTimer(formatTime(secondsLeft - 1))
+
+      return () => clearInterval(timerId)
+    }
+  })
+
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <h1>Mark Baptist Welcomes You</h1>
+      <div className={styles.timerContainer}>
+        <h1>{timer}</h1>
+      </div>      
+      <h1>The Service is about to Start</h1>
     </div>
   );
 }
